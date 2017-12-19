67be4bead74cf92b6a6780082ffad0c402f325f9
Tony van Loon <tony@3dimensions.nl>
2017-12-19 12:54:41 +0100

    First upload

---
+++ Marble Run Project/Assets/3Dimensions/Scripts/Editor/CustomAssetImporter.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+internal sealed class CustomAssetImporter : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        var importer = assetImporter as TextureImporter;
+
+        importer.alphaIsTransparency = importer.DoesSourceTextureHaveAlpha();
+
+    }
+
+    private void OnPreprocessModel()
+    {
+        var importer = assetImporter as ModelImporter;
+
+        // If it is static we don't want any kind of animation imported
+        importer.animationType = ModelImporterAnimationType.None;
+        importer.importAnimation = false;
+        importer.generateSecondaryUV = true;
+        importer.addCollider = true;
+
+
+        importer.materialSearch = ModelImporterMaterialSearch.Local;
+        importer.materialName = ModelImporterMaterialName.BasedOnMaterialName;
+    }
+}
+#endif
